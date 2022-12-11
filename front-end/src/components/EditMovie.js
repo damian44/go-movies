7df1e55682e65667feea4551c065d1228605e63c
damian44 <sergiovidela04@live.com>
2022-12-10 22:51:22 -0300

    initial prokect

---
+++ front-end/src/components/EditMovie.js
@@ -0,0 +1,12 @@
+const EditMovie = () => {
+
+    return(
+        <div className="text-center">
+            <h2>EditMovie</h2>
+            <hr />
+        </div>
+        
+    )
+}
+
+export default EditMovie;
